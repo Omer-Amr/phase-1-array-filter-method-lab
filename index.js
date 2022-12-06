0f11ed8e4a225b31fbc46d03d0c9616e510a27dd
omer <you@example.com>
2022-12-06 21:44:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, name){
+    return drivers.filter(
+      (sameDriverName) => sameDriverName.toLowerCase() === name.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(drivers, x){
+    return drivers.filter((driverName) => driverName.startsWith(x))
+}
+   
+function matchName(drivers, string){
+  return drivers.filter((driver) => driver.name === string)
+}
